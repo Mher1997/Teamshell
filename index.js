9e112e356e9dd577869fe3f07c81330bcbf3a55d
Mher <garanyan_97@mail.ru>
2020-07-30 00:30:56 +0400

    ver 0.0.1

---
+++ index.js
@@ -0,0 +1,11 @@
+$('#mob-menu-icon').click(function () {
+    $('#mobile-menu').css('display', 'block');
+})
+
+$('#menu-close').click(function () {
+    $('#mobile-menu').css('display', 'none');
+})
+
+$('#mobile-nav').click(function () {
+    $('#mobile-menu').css('display', 'none');
+})
