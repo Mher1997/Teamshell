db5c5ff0d161790c69ec264e3b4b502485428a04
Mher <garanyan_97@mail.ru>
2020-07-30 13:28:55 +0400

    ver 0.1.1

--- libs/index.js
+++ libs/index.js
@@ -11,19 +11,25 @@ $('#mobile-nav').click(function () {
 })
 
 const ua = navigator.userAgent.toLowerCase(); 
-if (ua.indexOf('safari') != -1) { 
-    if (ua.indexOf('chrome') == -1) {
-        $('.with-play-icon').each(function () {
-            $(this).addClass('safary-icon');
-        })
-    }
+let safari = ua.indexOf('safari') != -1 && ua.indexOf('chrome') == -1;
+
+if (safari) {
+    $('.with-play-icon').each(function () {
+        $(this).addClass('safary-icon');
+    })
 }
 
-// $(document).ready(function () {
-//     setTimeout(function () {
-//         const videos = document.getElementsByTagName('video');
-//         for(let i=0; i<videos.length; i++){
-//             videos[i].play();
-//         }
-//     })
-// }, 1000);
+if(safari){
+    const videos = $('.video');
+    for(let i=0; i<videos.length; i++){
+        let parent = $(videos[i]).parent();
+        let src = $(videos[i]).find('source').attr('src');
+        const videClass = $(videos[i]).attr('class').indexOf('sm-media');
+    
+        src = src.replace('mp4', 'gif');
+        parent.find('video').remove();
+        
+        let img = `<img src=${src} alt="gif" class=${videClass !==-1 ? 'sm-media' : ''}>`
+        parent.append(img);
+    }
+}
